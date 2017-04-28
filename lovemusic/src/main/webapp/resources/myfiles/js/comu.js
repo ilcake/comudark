f6ff3e6b7569221c2306c75f529dd2641c90d2ca
김태현 <dkwlsdl2@gmail.com>
2017-04-28 09:03:00 +0900

    다이죠부까나

--- lovemusic/src/main/webapp/resources/myfiles/js/comu.js
+++ lovemusic/src/main/webapp/resources/myfiles/js/comu.js
@@ -1,114 +1,223 @@
 var errorLine;
-var recentTA;
+var recentEditor;
+var sampleEditor;
+var mainEditor;
 $(function() {
+	setView();
 	setStringFormat();
 	getMusicTree();
-	
-	if (typeof jQuery != 'undefined') { 
-		// jQuery is loaded => print the version
-	 alert(jQuery.fn.jquery); 
-	 }
-	
-	setJqueryFn();
-	$("#myCarousel").css("height", "100%").css("padding-top", "2%");
+	setEditor();
+	setClickable();
+	/*
+	 * if (typeof jQuery != 'undefined') { // jQuery is loaded => print the
+	 * version alert(jQuery.fn.jquery); }
+	 */
+
 	$(".get-started").click(getStarted);
 	$("#addBtn").click(addBtn);
-	$(".saveBtn").click(save);
+	$("#saveBtn").click(save);
 	$("#imgInp").on('change', function() {
 		readURL(this);
 	});
-	$("#sampleRun").click(function() {
-		recentTA = $("#sample").get(0);
-		comuRun($("#sample").val());
-	});
-	$("#mainRun").click(function() {
-		recentTA = $("#main").get(0);
-		comuRun($("#main").val());
+
+	$("#runBtn").click(function() {
+		if (recentEditor == null) {
+			alert("sample or main?")
+			return;
+		}
+		$("#errorContent").html("<a href='#' id='errorClick'></a><br>");
+		comuRun(recentEditor.getValue());
 	});
 	$("#errorClick").click(function() {
-		selectTextareaLine(recentTA, errorLine);
+		selectTextareaLine(errorLine);
 	});
 
-	if ($("#title").val() != null && $("#title").val() > 0) {
-		$(".get-started").trigger("click");
-	}
 });
 
-function setStringFormat(){
-	if (!String.format) {
-		  String.format = function(format) {
-		    var args = Array.prototype.slice.call(arguments, 1);
-		    return format.replace(/{(\d+)}/g, function(match, number) { 
-		      return typeof args[number] != 'undefined'
-		        ? args[number] 
-		        : match
-		      ;
-		    });
-		  };
+function setClickable() {
+	$(".clickable").click(function() {
+		$(".clickable").each(function(index, item) {
+			$(item).removeClass("clicked");
+		});
+		var tmp = $(this).attr("data-clicked");
+		switch (tmp) {
+		case "list-explorer":
+			$(".list-explorer").addClass("clicked");
+			recentEditor = null;
+			break;
+		case "sample":
+			$(".sample").addClass("clicked");
+			recentEditor = sampleEditor;
+			break;
+		case "main":
+			$(".main").addClass("clicked");
+			recentEditor = mainEditor;
+			break;
+		case "error":
+			$(".errorTag").addClass("clicked");
+			recentEditor = null;
+			break;
 		}
+	});
+}
+
+function setView() {
+	$("#totalWrapper").css("height", $(window).height()).css("padding-top",
+			"90px");
+}
+
+function setEditor() {
+	define(
+			"DynHighlightRules",
+			[],
+			function(require, exports, module) {
+				"use strict";
+				var oop = require("ace/lib/oop");
+				var TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
+				var DynHighlightRules = function() {
+					this.keywordRule = {
+						regex : "\\w+",
+						onMatch : function() {
+							return "text"
+						}
+					}
+					this.$rules = {
+						"start" : [
+								{
+									token : "keyword",
+									regex : "tempo|bpm|loop|ins"
+								},
+								{
+									token : "variable",
+									regex : "location|do|note|reverb|delay|low|high"
+								},
+								{
+									token : "constant",
+									regex : "bass|beat|melody|acu|dub|guitarcode|guitarnote|piano|r8"
+								}, {
+									token : "markup.heading",
+									regex : "[_A-Za-z$][_A-Za-z0-9$]*"
+								}, {
+									token : "comment",
+									regex : "[0-9]+"
+								}, this.keywordRule ]
+					};
+					this.normalizeRules()
+				};
+				oop.inherits(DynHighlightRules, TextHighlightRules);
+				exports.DynHighlightRules = DynHighlightRules;
+			});
+
+	var TextMode = require("ace/mode/text").Mode;
+	var dynamicMode = new TextMode();
+	dynamicMode.HighlightRules = require("DynHighlightRules").DynHighlightRules;
+
+	sampleEditor = ace.edit("sampleEditor");
+	sampleEditor.resize();
+	sampleEditor.setTheme("ace/theme/vibrant_ink");
+	sampleEditor.session.setMode(dynamicMode);
+	document.getElementById('sampleEditor').style.fontSize = '18px';
+
+	mainEditor = ace.edit("mainEditor");
+	mainEditor.resize();
+	mainEditor.setTheme("ace/theme/vibrant_ink");
+	mainEditor.session.setMode(dynamicMode);
+	document.getElementById('mainEditor').style.fontSize = '18px';
+
+}
+
+function setStringFormat() {
+	if (!String.format) {
+		String.format = function(format) {
+			var args = Array.prototype.slice.call(arguments, 1);
+			return format.replace(/{(\d+)}/g, function(match, number) {
+				return typeof args[number] != 'undefined' ? args[number]
+						: match;
+			});
+		};
+	}
 }
 
 function getMusicTree() {
-	$.ajax({
-		type : "post",
-		url : "getList",
-		success : function(resp) {
-			var tree = '{ "data" : [';
-			var mapKey = Object.keys(resp);
-			var insId = 0;
-			var arr = [];
-			$.each(mapKey, function(index, item) {
-				// console.log(JSON.stringify(resp[item][0]));
-				// console.log(resp[item][0]["motherName"]);
-				// console.log(resp[item][0]["insName"]);
-				// console.log(resp[item][0]["fileName"]);
-				// tree += String.format("{'id' : '{0}' , 'parent' : '#', 'text'
-				// : '{1}' }", );
-				if($.inArray(resp[item][0]["motherName"],arr) == -1){
-					if(index != 0){
-						tree += ", "
-					}
-					tree += String.format('{"id" : "{0}" ,"parent" : "#", "text" : "{1}"}', resp[item][0]["motherName"], resp[item][0]["motherName"]);
-					arr.push(resp[item][0]["motherName"]);
-				}
-				if(resp[item][0]["insName"] != 'effect'){
-					tree += String.format(', {"id" : "{0}" , "parent" : "{1}", "text" : "{2}" }', resp[item][0]["insName"], resp[item][0]["motherName"], resp[item][0]["insName"]);
-				}
-				var arrayList = resp[item];
-				$.each(arrayList, function(listIndex, listItem) {
-					var id = insId++;
-					var parent  = listItem["insName"];
-					var text = listItem["fileName"];
-					tree += String.format(', {"id" : "{0}" , "parent" : "{1}", "text" : "{2}" }', id, parent, text);
-				});
-			});
-			console.log(arr);
-			tree += "] }";
-			console.log(tree);
-			var jsonTree = JSON.parse(tree);
-			console.log(jsonTree);
-			$('#treeViewDiv').jstree({
-				'plugins' : [ "wholerow" ],
-				'core' : jsonTree
+	$
+			.ajax({
+				type : "post",
+				url : "getList",
+				success : function(resp) {
+					var tree = '{ "data" : [';
+					var mapKey = Object.keys(resp);
+					var insId = 0;
+					var arr = [];
+					$
+							.each(
+									mapKey,
+									function(index, item) {
+										// console.log(JSON.stringify(resp[item][0]));
+										// console.log(resp[item][0]["motherName"]);
+										// console.log(resp[item][0]["insName"]);
+										// console.log(resp[item][0]["fileName"]);
+										// tree += String.format("{'id' : '{0}'
+										// , 'parent' : '#', 'text'
+										// : '{1}' }", );
+										if ($.inArray(
+												resp[item][0]["motherName"],
+												arr) == -1) {
+											if (index != 0) {
+												tree += ", "
+											}
+											tree += String
+													.format(
+															'{"id" : "{0}" ,"parent" : "#", "text" : "{1}"}',
+															resp[item][0]["motherName"],
+															resp[item][0]["motherName"]);
+											arr
+													.push(resp[item][0]["motherName"]);
+										}
+										if (resp[item][0]["insName"] != 'effect') {
+											tree += String
+													.format(
+															', {"id" : "{0}" , "parent" : "{1}", "text" : "{2}" }',
+															resp[item][0]["insName"],
+															resp[item][0]["motherName"],
+															resp[item][0]["insName"]);
+										}
+										var arrayList = resp[item];
+										$
+												.each(
+														arrayList,
+														function(listIndex,
+																listItem) {
+															var id = insId++;
+															var parent = listItem["insName"];
+															var text = listItem["fileName"];
+															tree += String
+																	.format(
+																			', {"id" : "{0}" , "parent" : "{1}", "text" : "{2}" }',
+																			id,
+																			parent,
+																			text);
+														});
+									});
+					tree += "] }";
+					var jsonTree = JSON.parse(tree);
+					$('#treeViewDiv').jstree({
+						'plugins' : [ "wholerow" ],
+						'core' : jsonTree
 					});
-		}
-	});
+				}
+			});
 }
 
 // 시작 버튼이 눌러졌을 때 혹은 MY MUSIC에서 로드 될 때
 function getStarted() {
 	$(".comu-container").show("slow");
 	$(this).attr("hidden", "");
-	$(".btn-default").css("background-color", "rgb(39,169,157)");
-	$("#addBtn").css("background-color", "#f0ad4e");
 }
 
 function addBtn() {
-	var sample = $('#sample').val();
-	var position = $("#main").getCursorPosition();
-	$("#main").setCursorPosition(position);
-	$("#main").insertAtCursor(sample);
-	$('#sample').val('');
+	var sample = sampleEditor.getValue();
+	mainEditor.insert(sample);
+	sampleEditor.setValue("");
 }
 
 function comuRun(source) {
@@ -123,13 +232,16 @@ function comuRun(source) {
 			"source" : source
 		},
 		success : function(resp) {
+			console.log("resp = " + resp);
 			var check = resp.substring(0, 5);
 			if (check == "error") {
 				var errorMsg = resp.substring(5, resp.length);
+				console.log("errorMsg = " + errorMsg);
 				errorLine = parseInt(errorMsg);
-				$("#errorClick").html("errorLine :  " + errorLine);
-				$("#errorContent").html(
-						errorMsg.substring(errorLine, errorMsg.length));
+				errorMsg = errorMsg.substring((errorLine + "").length + 4,
+						errorMsg.length);
+				$("#errorClick").html("Error Line :  " + errorLine);
+				$("#errorContent").append(errorMsg);
 			} else {
 				$("#modalBtn").trigger("click");
 				eval(resp);
@@ -157,111 +269,10 @@ function save() {
 	$("#form").submit();
 }
 
-// 외부 jqeury 함수 셋팅
-function setJqueryFn() {
-	$.fn.setCursorPosition = function(position) {
-		if (this.length == 0)
-			return this;
-		return $(this).setSelection(position, position);
-	}
-	$.fn.setSelection = function(selectionStart, selectionEnd) {
-		if (this.length == 0)
-			return this;
-		input = this[0];
-		if (input.createTextRange) {
-			var range = input.createTextRange();
-			range.collapse(true);
-			range.moveEnd('character', selectionEnd);
-			range.moveStart('character', selectionStart);
-			range.select();
-		} else if (input.setSelectionRange) {
-			input.focus();
-			input.setSelectionRange(selectionStart, selectionEnd);
-		}
-		return this;
-	}
-	$.fn.focusEnd = function() {
-		this.setCursorPosition(this.val().length);
-		return this;
-	}
-	$.fn.getCursorPosition = function() {
-		var el = $(this).get(0);
-		var pos = 0;
-		if ('selectionStart' in el) {
-			pos = el.selectionStart;
-		} else if ('selection' in document) {
-			el.focus();
-			var Sel = document.selection.createRange();
-			var SelLength = document.selection.createRange().text.length;
-			Sel.moveStart('character', -el.value.length);
-			pos = Sel.text.length - SelLength;
-		}
-		return pos;
-	}
-	$.fn.insertAtCursor = function(myValue) {
-		return this.each(function(i) {
-			if (document.selection) {
-				// For browsers like Internet Explorer
-				this.focus();
-				sel = document.selection.createRange();
-				sel.text = myValue;
-				this.focus();
-			} else if (this.selectionStart || this.selectionStart == '0') {
-				// For browsers like Firefox and Webkit based
-				var startPos = this.selectionStart;
-				var endPos = this.selectionEnd;
-				var scrollTop = this.scrollTop;
-				this.value = this.value.substring(0, startPos) + myValue
-						+ this.value.substring(endPos, this.value.length);
-				this.focus();
-				this.selectionStart = startPos + myValue.length;
-				this.selectionEnd = startPos + myValue.length;
-				this.scrollTop = scrollTop;
-			} else {
-				this.value += myValue;
-				this.focus();
-			}
-		})
-	}
-}
-
 // 에러 메세지 클릭시 해당 하는 라인 셀렉트
-function selectTextareaLine(tarea, lineNum) {
-	lineNum--; // array starts at 0
-	var lines = tarea.value.split("\n");
-
-	// calculate start/end
-	var startPos = 0, endPos = tarea.value.length;
-	for (var x = 0; x < lines.length; x++) {
-		if (x == lineNum) {
-			break;
-		}
-		startPos += (lines[x].length + 1);
-
-	}
-
-	var endPos = lines[lineNum].length + startPos;
-
-	// do selection
-	// Chrome / Firefox
-
-	if (typeof (tarea.selectionStart) != "undefined") {
-		tarea.focus();
-		tarea.selectionStart = startPos;
-		tarea.selectionEnd = endPos;
-		return true;
-	}
-
-	// IE
-	if (document.selection && document.selection.createRange) {
-		tarea.focus();
-		tarea.select();
-		var range = document.selection.createRange();
-		range.collapse(true);
-		range.moveEnd("character", endPos);
-		range.moveStart("character", startPos);
-		range.select();
-		return true;
-	}
-	return false;
+function selectTextareaLine(lineNum) {
+	var Range = require("ace/range").Range;
+	console.log(new Range(lineNum, 0, lineNum, 9999));
+	recentEditor.selection
+			.setRange(new Range(lineNum - 1, 0, lineNum - 1, 9999));
 }
