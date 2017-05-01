cb006b5f8ece3d908003a10daf6f8e53458db470
ilcake <yucheol@gmail.com>
2017-05-01 17:34:27 +0900

    changed to CLOB / comu modal reDesign

--- lovemusic/src/main/webapp/resources/myfiles/js/comu.js
+++ lovemusic/src/main/webapp/resources/myfiles/js/comu.js
@@ -98,6 +98,12 @@ function save() {
 		return;
 	}
 	var txt = mainEditor.getValue();
+	var reTxt = txt.split("\n");
+	var ggTxt = "";
+	$.each(reTxt, function(index, item) {
+		ggTxt += item += "*";
+	});
+	txt = ggTxt;
 
 	$('<input>').attr({
 		type : 'text',
@@ -110,6 +116,13 @@ function save() {
 function load() {
 	$('.loadable').click(function() {
 		source = $(this).attr('data-file_ori');
+		var reSource = source.split("*");
+		var comp = "";
+		$.each(reSource, function(index, item) {
+			comp += item + "\n";
+		});
+		source = comp;
+
 		$('.loadable').each(function(index, item) {
 			$(item).removeClass('selected')
 		})
